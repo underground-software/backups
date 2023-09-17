5c82533b0c49710c3ce4e44962f09d5526b6b33a
Joel Savitz <jsavitz@redhat.com>
2023-09-17 14:18:05 -0400

    add grades.db to automated backup

--- do_local_backup.sh
+++ do_local_backup.sh
@@ -5,3 +5,7 @@ echo "KDLP ORBIT LOCAL DATA BACKUP SCRIPT"
 echo "[1]: back up users.db from venus"
 
 cp /mnt/telomerase/cano.py/venus/users.db /mnt/telomerase/backups/users.db.$(date +%s)
+
+echo "[2]: back up grades.db from mercury"
+
+cp /mnt/telomerase/cano.py/mercury/grades.db /mnt/telomerase/backups/grades.db.$(date +%s)
