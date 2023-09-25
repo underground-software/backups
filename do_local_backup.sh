8b04adb41d4f2a6e6190fe53235805eb4ef24723
Joel Savitz <jsavitz@redhat.com>
2023-09-24 23:00:22 -0400

    script runs from anywhere
    Signed-off-by: Joel Savitz <jsavitz@redhat.com>

--- do_local_backup.sh
+++ do_local_backup.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
+PREFIX='/mnt/telomerase'
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TIMESTAMP=${1:-$(date +%s)}
 
 echo "KDLP ORBIT LOCAL DATA BACKUP SCRIPT @ $TIMESTAMP"
 
 echo "[1]: back up users.db from venus"
 
-cp /mnt/telomerase/cano.py/venus/users.db /mnt/telomerase/backups/users.db.$TIMESTAMP
+cp $PREFIX/cano.py/venus/users.db $SCRIPT_DIR/users.db.$TIMESTAMP
 
 echo "[2]: back up grades.db from mercury"
 
-cp /mnt/telomerase/cano.py/mercury/grades.db /mnt/telomerase/backups/grades.db.$TIMESTAMP
+cp $PREFIX/cano.py/mercury/grades.db $SCRIPT_DIR/grades.db.$TIMESTAMP
 
 echo "[3]: back up email_data"
 
-DEST=/mnt/telomerase/backups/email_data.$TIMESTAMP
+DEST=$PREFIX/backups/email_data.$TIMESTAMP
 mkdir -p $DEST
-cp -r /mnt/telomerase/email_data $DEST
+cp -r $PREFIX/email_data $DEST
 
 echo "[4]: output archive kdlp-prod-backup.$TIMESTAMP.tar.gz"
+cd $SCRIPT_DIR
 tar cf kdlp-prod-backup.$TIMESTAMP.tar.gz users.db.$TIMESTAMP grades.db.$TIMESTAMP email_data.$TIMESTAMP
