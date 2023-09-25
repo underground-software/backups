1a186015dd205d273ba92b222d51400d41592dd3
Joel Savitz <jsavitz@redhat.com>
2023-09-24 22:28:20 -0400

    backup email_data entirely
    Signed-off-by: Joel Savitz <jsavitz@redhat.com>

--- do_local_backup.sh
+++ do_local_backup.sh
@@ -9,3 +9,9 @@ cp /mnt/telomerase/cano.py/venus/users.db /mnt/telomerase/backups/users.db.$(dat
 echo "[2]: back up grades.db from mercury"
 
 cp /mnt/telomerase/cano.py/mercury/grades.db /mnt/telomerase/backups/grades.db.$(date +%s)
+
+echo "[3]: back up email_data"
+
+DEST=/mnt/telomerase/backups/email_data.$(date +%s)
+mkdir -p $DEST
+cp -r /mnt/telomerase/email_data $DEST
