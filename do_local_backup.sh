d7da91255a71c98cae61b3b2ae6ef2960805842b
Joel Savitz <joel@dev.underground.software>
2023-09-25 00:33:06 -0400

    preserve permissions and ownership on files in backup (pop was broken)
    Signed-off-by: Joel Savitz <joel@dev.underground.software>

--- do_local_backup.sh
+++ do_local_backup.sh
@@ -8,17 +8,17 @@ echo "KDLP ORBIT LOCAL DATA BACKUP SCRIPT @ $TIMESTAMP"
 
 echo "[1]: back up users.db from venus"
 
-cp $PREFIX/cano.py/venus/users.db $SCRIPT_DIR/users.db.$TIMESTAMP
+cp -a $PREFIX/cano.py/venus/users.db $SCRIPT_DIR/users.db.$TIMESTAMP
 
 echo "[2]: back up grades.db from mercury"
 
-cp $PREFIX/cano.py/mercury/grades.db $SCRIPT_DIR/grades.db.$TIMESTAMP
+cp -a $PREFIX/cano.py/mercury/grades.db $SCRIPT_DIR/grades.db.$TIMESTAMP
 
 echo "[3]: back up email_data"
 
 DEST=$PREFIX/backups/email_data.$TIMESTAMP
 mkdir -p $DEST
-cp -r $PREFIX/email_data $DEST
+cp -ar $PREFIX/email_data $DEST
 
 echo "[4]: output archive kdlp-prod-backup.$TIMESTAMP.tar.gz"
 cd $SCRIPT_DIR
