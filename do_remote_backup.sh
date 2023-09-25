fe29a1ca1f39affea24118637b2171bec3223e4b
Joel Savitz <joelsavitz@gmail.com>
2023-09-24 21:41:16 -0400

    remote backup is even better
    Signed-off-by: Joel Savitz <joelsavitz@gmail.com>

--- do_remote_backup.sh
+++ do_remote_backup.sh
@@ -3,5 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TIMESTAMP=$(date +%s)
 PREFIX='/mnt/telomerase'
-ssh root@dev-01.kdlp.underground.software $PREFIX/backups/do_local_backup.sh "$TIMESTAMP"
-scp root@dev-01.kdlp.underground.software:$PREFIX/backups/kdlp-prod-backup.$TIMESTAMP.tar.gz $SCRIPT_DIR
+REMOTE='dev-01.kdlp.underground.software'
+ssh root@$REMOTE $PREFIX/backups/do_local_backup.sh "$TIMESTAMP"
+scp root@$REMOTE:$PREFIX/backups/kdlp-prod-backup.$TIMESTAMP.tar.gz \
+	$SCRIPT_DIR/kdlp-$REMOTE-backup.$TIMESTAMP.tar.gz
