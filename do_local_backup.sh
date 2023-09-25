a6e2b035d45dff1806d3a59d7842b9a7c51004be
Joel Savitz <jsavitz@redhat.com>
2023-09-24 22:32:59 -0400

    consolidate backups into archive
    Signed-off-by: Joel Savitz <jsavitz@redhat.com>

--- do_local_backup.sh
+++ do_local_backup.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-echo "KDLP ORBIT LOCAL DATA BACKUP SCRIPT"
+
+TIMESTAMP=$(date +%s)
+
+echo "KDLP ORBIT LOCAL DATA BACKUP SCRIPT @ $TIMESTAMP"
 
 echo "[1]: back up users.db from venus"
 
-cp /mnt/telomerase/cano.py/venus/users.db /mnt/telomerase/backups/users.db.$(date +%s)
+cp /mnt/telomerase/cano.py/venus/users.db /mnt/telomerase/backups/users.db.$TIMESTAMP
 
 echo "[2]: back up grades.db from mercury"
 
-cp /mnt/telomerase/cano.py/mercury/grades.db /mnt/telomerase/backups/grades.db.$(date +%s)
+cp /mnt/telomerase/cano.py/mercury/grades.db /mnt/telomerase/backups/grades.db.$TIMESTAMP
 
 echo "[3]: back up email_data"
 
-DEST=/mnt/telomerase/backups/email_data.$(date +%s)
+DEST=/mnt/telomerase/backups/email_data.$TIMESTAMP
 mkdir -p $DEST
 cp -r /mnt/telomerase/email_data $DEST
+
+echo "[4]: output archive kdlp-prod-backup.$TIMESTAMP.tar.gz"
+tar cf kdlp-prod-backup.$TIMESTAMP.tar.gz users.db.$TIMESTAMP grades.db.$TIMESTAMP email_data.$TIMESTAMP
